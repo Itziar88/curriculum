74460c6311ae2aa93ac4f1b6b71cdf89a5a268bd
Itziar Puebla <itziarpuebla@gmail.com>
2020-04-01 14:32:15 +0200

    icon, text and section components created

---
+++ src/components/Section.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="Section">
+        <CText 
+            typo="h2"
+            weight="semi"
+            uppercase
+        >
+            {{ title }}
+        </CText>
+    </div>
+</template>
+
+<script>
+import VueTypes from 'vue-types'
+import { CText } from '@/components'
+
+export default {
+    name: 'Section',
+    components: {
+        CText,
+    },
+    props: {
+        title: VueTypes.string.def(''),
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/theme/theme.scss';
+
+.Section {
+    padding-bottom: 5px;
+    position: relative;
+    &::after {
+        content: '';
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 4px;
+        border-radius: 5px;
+        background-color: $primary;
+    }
+}
+</style>
